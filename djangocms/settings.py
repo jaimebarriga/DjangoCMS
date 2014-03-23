993ff9735d8495daf291a22841ac800720c83570
Jaime Barriga <jab.colo33@gmail.com>
2014-03-23 05:09:34 -0400

    change procfile

--- djangocms/settings.py
+++ djangocms/settings.py
@@ -14,6 +14,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#WHEN IN HEROKU
+# Parse database configuration from $DATABASE_URL
+if('DATABASE_URL' in os.environ):
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
