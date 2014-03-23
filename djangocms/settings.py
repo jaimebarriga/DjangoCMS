5b9654cf7cd793d5b387311b9e2530b193268e6d
Jaime Barriga <jab.colo33@gmail.com>
2014-03-23 05:13:38 -0400

    fix database settings for heroku

--- djangocms/settings.py
+++ djangocms/settings.py
@@ -18,26 +18,27 @@ MANAGERS = ADMINS
 # Parse database configuration from $DATABASE_URL
 if('DATABASE_URL' in os.environ):
     import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
+    DATABASES = {
+        'default' : dj_database_url.config()
+    }
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow all host headers
     ALLOWED_HOSTS = ['*']
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'sqlite3.db'), # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(PROJECT_PATH, 'sqlite3.db'), # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
     }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
